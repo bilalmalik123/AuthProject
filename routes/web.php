da83602c9daaab1e26afc36674f03750f7c21ac2
bilal <bilalmlk3@gmail.com>
2019-11-07 15:27:49 +0530

    Auth project

---
+++ routes/web.php
@@ -0,0 +1,69 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+// Authentication Routes...
+// $this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
+// $this->post('login', 'Auth\LoginController@login');
+// $this->post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// // Registration Routes...
+// $this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+// $this->post('register', 'Auth\RegisterController@register');
+
+// // Password Reset Routes...
+// $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm');
+// $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
+// $this->get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm');
+// $this->post('password/reset', 'Auth\ResetPasswordController@reset');\
+
+// Route::get('/home', 'HomeController@index');
+
+
+Route::get('/admin', 'AdminController@index');
+Route::get('/adminPage1', 'AdminController@page1');
+Route::get('/adminPage2', 'AdminController@page1');
+
+
+Route::get('/superadmin', 'SuperAdminController@index');
+Route::get('/superadminPage1', 'SuperAdminController@page1');
+
+
+Route::get('/user', 'UserController@index');
+Route::get('/userPage1', 'UserController@page1');
+//Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+
+Route::group(['middleware' => 'auth'], function () {
+		Route::get('icons', ['as' => 'pages.icons', 'uses' => 'PageController@icons']);
+		Route::get('maps', ['as' => 'pages.maps', 'uses' => 'PageController@maps']);
+		Route::get('notifications', ['as' => 'pages.notifications', 'uses' => 'PageController@notifications']);
+		Route::get('rtl', ['as' => 'pages.rtl', 'uses' => 'PageController@rtl']);
+		Route::get('tables', ['as' => 'pages.tables', 'uses' => 'PageController@tables']);
+		Route::get('typography', ['as' => 'pages.typography', 'uses' => 'PageController@typography']);
+		Route::get('upgrade', ['as' => 'pages.upgrade', 'uses' => 'PageController@upgrade']);
+});
+
+Route::group(['middleware' => 'auth'], function () {
+	Route::resource('user', 'UserController', ['except' => ['show']]);
+	Route::get('profile', ['as' => 'profile.edit', 'uses' => 'ProfileController@edit']);
+	Route::put('profile', ['as' => 'profile.update', 'uses' => 'ProfileController@update']);
+	Route::put('profile/password', ['as' => 'profile.password', 'uses' => 'ProfileController@password']);
+});
+
